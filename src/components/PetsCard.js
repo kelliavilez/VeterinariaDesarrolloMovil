d06bb1ede3f71a8d6d5b460b40e023abbbf4eb2d
Juliana <julianat1514@gmail.com>
2024-08-19 21:16:02 -0500

    Solución error botón

--- src/components/PetsCard.js
+++ src/components/PetsCard.js
@@ -1,4 +1,32 @@
 import React from 'react';
+import { View, Text, Image, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import styles from '../styles/globalStyles';
+
+const PetsCard = ({ pet }) => {
+  const navigation = useNavigation();
+
+  return (
+    <View style={styles.card}>
+      <Image source={{ uri: pet.photo }} style={styles.photo} />
+      <Text style={styles.name}>Nombre Mascota: {pet.name}</Text>
+      <Text>Raza: {pet.race}</Text>
+      <Text>Tipo: {pet.type}</Text>
+      <Text>Edad: {pet.age}</Text>
+      <Text>Propietario: {pet.person}</Text>
+      <Text>Veterinario: {pet.vet}</Text>
+
+      <Button
+        title="Agendar cita"
+        style={styles.button}
+        onPress={() => navigation.navigate('AppoinmentPets')}
+      />
+    </View>
+  );
+};
+
+export default PetsCard;
+/*import React from 'react';
 import {View, Text, Image} from 'react-native'
 import { Button } from 'react-native';
 import styles from '../styles/globalStyles';
@@ -24,5 +52,5 @@ const PetsCard = ({pet})=>{
     );
 };
 
-export default PetsCard;
+export default PetsCard;*/
 
